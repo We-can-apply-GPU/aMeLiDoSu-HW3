74e3210a5416a4edc4f4741fa67711a13e448f7b
JieFang831116 <b02901013@ntu.edu.tw>
2015-05-22 16:47:41 +0800

    new

--- iodata.py
+++ iodata.py
@@ -7,23 +7,22 @@ Description: produce in, out data for train
 
 import extract
 import sentence
+from settings import *
 
-Ngram = 3
-batchsize = 100
 indatalist = []
 outdatalist = []
 
 def iodata():
     _list = sentence.getsen('swear')
-    #dic = extract.extract(100, 'train_pro.txt', 'true')
+    dic = extract.extract(100, 'train_pro.txt', 'true')
     for i in range(0, len(_list)):
         inlist = []
         outlist = []
         for j in range(0, len(_list[i])):
             _str = _list[i][j].split()
             for k in range(0, len(_str)):
-                if(len(_str)>Ngram):
-                    if((len(_str)-k)>Ngram):
+                if(len(_str)>NGRAMS):
+                    if((len(_str)-k)>NGRAMS):
                         #indata = [dic[_str[k]],dic[_str[k+1]],dic[_str[k+2]]]
                         #outdata = [dic[_str[k+1]],dic[_str[k+2]],dic[_str[k+3]]]
                         indata = [_str[k+0],_str[k+1],_str[k+2]]
@@ -31,7 +30,7 @@ def iodata():
                         inlist.append(indata)
                         outlist.append(outdata)
         index = 0
-        while(len(inlist)<batchsize):
+        while(len(inlist)<BATCH_SIZE):
             inlist.append(inlist[index])
             outlist.append(outlist[index])
             index = index + 1
