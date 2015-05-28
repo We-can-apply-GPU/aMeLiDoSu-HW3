a01e777fab844041c1d41e35703f2dd829349fc5
b02901088 <b02901088@linux8.csie.ntu.edu.tw>
2015-05-28 22:43:05 +0800

    for alltest

---
+++ sentence2.py
@@ -0,0 +1,21 @@
+def getsen (s,size=5):
+    d = open(s)
+    ls = []
+    count = 0
+    lsofls=[]
+    for i in d:
+        i = i.rstrip()
+        j = i.split()
+        if True:
+            count+= len(j)-3
+            if count> batchsize:
+                lsofls.append(ls)
+                ls = []
+                count = len(j)-3
+            ls.append(i)
+    if len(ls) != 0:
+        lsofls.append(ls)
+    return lsofls
+batchsize = 100
+j = getsen('alltest')
+print(len(j))
